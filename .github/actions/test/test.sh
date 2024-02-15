7468ec15292789e4c06ea460556ed367f3a10f85
Mohcine Tor <mohcine.tor@cosmotech.com>
2024-02-15 23:06:48 +0100

    MTOR/add pull action

--- .github/actions/test/test.sh
+++ .github/actions/test/test.sh
@@ -5,3 +5,8 @@ platforms=("dev" "perf")
 for platform in "${platforms[@]}"; do
     echo "$platform"
 done
+
+        IFS=',' read -ra platforms_array <<< "${{ inputs.platforms }}"
+        for platform in "${platforms_array[@]}"; do
+          echo "$platform"
+        done
